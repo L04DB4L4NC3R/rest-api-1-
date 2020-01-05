7fbed8cdc24bdff97b2aaa19dc3378ff87fd9a5b
Pragati1610 <55177805+Pragati1610@users.noreply.github.com>
2020-01-05 09:26:57 +0530

    app updated

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
 const express = require('express');
 const app = express();
-const productRouter = require('./api/routes/products');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use('/products',productRouter);
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://Pragati:' + process.env.MONGO_ATLAS_PW + '@shop-rest-api-eexlz.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useMongoClient : true
+}
+);
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');//not send the res just adjusts it //adding headers
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept,Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});    
+    }
+});
+
+
+
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
