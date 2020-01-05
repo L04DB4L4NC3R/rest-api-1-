1015f72b6530971afe83c7eab988aedaaab82528
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-01-05 12:26:34 +0530

    removed DEBUG statement from router

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,7 +12,6 @@ router.get('/',(req,res,next) => {
 
 
 router.post('/',(req,res,next) => {
-				console.log("DEBUG")
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
